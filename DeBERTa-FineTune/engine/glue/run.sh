7549892b174cdaa90ca0c244cffb2e57c4c4af6a
weicai <wei.cai@moffett.ai>
2021-11-29 10:35:36 +0800

    run script

--- DeBERTa-FineTune/engine/glue/run.sh
+++ DeBERTa-FineTune/engine/glue/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 TASK=rte
-MODEL=microsoft/deberta-base-mnli
+MODEL=deberta-base-mnli
 EPOCHS=24
 WARMUP_STEPS=100
 LR=4e-6
 PRUNE_FREQ=10
-NOHUP_OUTPUT=outputs/$TASK/ep-$EPOCHS_lr-$LR_freq-$PRUNE_FREQ.log
+NOHUP_OUTPUT=output/$TASK/$MODEL/ep$EPOCHS-lr$LR-freq$PRUNE_FREQ.log
 
 OMP_NUM_THREADS=1
 CUDA_VISIBLE_DEVICES=6,7
 TOKENIZERS_PARALLELISM=true nohup
 
-accelerate launch run_glue.py --task_name $TASK --model_type $MODEL --train_batch_size 8 --val_batch_size 8 --epochs $EPOCHS --warmup_steps $WARMUP_STEPS --lr $LR --max_seq_length 320 --pad_to_max_seq_length --cls_dropout 0.2 --weight_decay 0 --pruning --prune_frequency $PRUNE_FREQ >> $NOHUP_OUTPUT 2>&1 &
+accelerate launch run_glue.py --task_name $TASK --model_type microsoft/$MODEL --train_batch_size 8 --val_batch_size 8 --epochs $EPOCHS --warmup_steps $WARMUP_STEPS --lr $LR --max_seq_length 320 --pad_to_max_seq_length --cls_dropout 0.2 --weight_decay 0 --pruning --prune_frequency $PRUNE_FREQ >> $NOHUP_OUTPUT 2>&1 &
